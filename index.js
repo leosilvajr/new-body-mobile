58e081325ad6cf2150fb984b5f659964cd742780
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-06-08 23:02:47 -0300

    Preparando Aplicação para o primeiro deploy

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ import App from './App';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+// node version 22.14.0
+
+//eas --version
+// eas-cli/16.9.0 win32-x64 node-v22.14.0
 registerRootComponent(App);
